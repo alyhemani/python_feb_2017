b80a738c0bd0907b5738e44002d84acda4f8a5e0
Aly Hemani <alyhemani@gmail.com>
2017-02-07 16:29:59 -0600

    todays assignments

---
+++ Aly_Hemani/dojosurvey/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+	return render_template('survey.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+	name = request.form['name']
+	location = request.form['location']
+	language = request.form['language']
+	comment = request.form['message']
+	return render_template('result.html', name= name, location= location, language= language, comment= comment)
+
+app.run(debug =True)
---
+++ Aly_Hemani/landingpage/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+	return render_template('index.html', name='Aly')
+
+@app.route('/ninjas')
+def ninjas():
+	return render_template('ninjas.html')
+
+@app.route('/dojos/new')
+def form():
+	return render_template('dojos.html')
+
+app.run(debug =True)
